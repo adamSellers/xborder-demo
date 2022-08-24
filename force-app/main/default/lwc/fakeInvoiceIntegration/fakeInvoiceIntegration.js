0d4932fcd272ff2efea833c6d303297c8b8f9316
Adam Sellers <asellers@aseller-ltmu9dh.internal.salesforce.com>
2022-08-24 14:01:27 +1000

    automation bits

---
+++ force-app/main/default/lwc/fakeInvoiceIntegration/fakeInvoiceIntegration.js
@@ -0,0 +1,38 @@
+import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { CloseActionScreenEvent } from 'lightning/actions';
+import demoDataCannon from '@salesforce/apex/CreateDemoInvoiceData.demoDataCannon';
+
+export default class FakeInvoiceIntegration extends LightningElement {
+
+    @api recordId;
+    spinner;
+
+    renderedCallback() {
+        console.log(`quick action component rendered with recordId: ${this.recordId}`);
+    }
+    // setup imperative apex call to respond to button click
+    createInvoiceData() {
+        this.spinner = true;
+        demoDataCannon({ acctId: this.recordId})
+            .then( result => {
+                this.spinner = false;
+                this.dispatchEvent(new CloseActionScreenEvent());
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Data Retrieved',
+                        message: `${result} Invoice(s) retrieved.`,
+                        variant: 'success'
+                    })
+                );
+            })
+            .catch( error => {
+                console.log(`there was an error: ${JSON.stringify(error)}`);
+            })
+    }
+
+    handleCancel() {
+
+        this.dispatchEvent(new CloseActionScreenEvent({bubbles:true, composed: true}));
+    }
+}
